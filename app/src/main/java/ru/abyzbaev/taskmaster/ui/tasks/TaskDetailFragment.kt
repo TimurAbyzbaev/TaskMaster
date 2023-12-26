f67a41227030457c9b0cb14642030975e9703bda
Timur <timur@vlaga.pro>
2023-12-26 08:26:22 +0500

    Добавил DatePicker, отображение выбранной даты в details, изменение выбранной даты, сохранение выбранной даты.

--- app/src/main/java/ru/abyzbaev/taskmaster/ui/tasks/TaskDetailFragment.kt
+++ app/src/main/java/ru/abyzbaev/taskmaster/ui/tasks/TaskDetailFragment.kt
@@ -1,6 +1,8 @@
 package ru.abyzbaev.taskmaster.ui.tasks
 
+import android.app.DatePickerDialog
 import android.content.Context
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +19,8 @@ import ru.abyzbaev.taskmaster.R
 import ru.abyzbaev.taskmaster.app.TaskMasterApplication
 import ru.abyzbaev.taskmaster.data.model.TaskEntity
 import ru.abyzbaev.taskmaster.databinding.FragmentTaskDetailBinding
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 class TaskDetailFragment : Fragment() {
@@ -31,6 +35,8 @@ class TaskDetailFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var task: TaskEntity
+
+    private var selectedDate: Long = 1L
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -83,24 +89,28 @@ class TaskDetailFragment : Fragment() {
         binding.buttonSave.setOnClickListener {
             saveTask()
         }
+        binding.dueDate.setOnClickListener {
+            showDatePicker()
+        }
+        binding.dueDate.text = formatDateFromLong(task.dueDate)
         binding.titleTask.setText(task.title)
         binding.categoryTask.setText(task.categoryId.toString())
         binding.descriptionTask.setText(task.description)
     }
 
     private fun saveTask() {
-        val title: String = view?.findViewById<EditText>(R.id.title_task)?.text.toString()
-        val description: String =
-            view?.findViewById<EditText>(R.id.description_task)?.text.toString()
+        val title: String = binding.titleTask.text.toString()
+        val description: String = binding.descriptionTask.text.toString()
+        val dueDate: Long = selectedDate
         val category: Long = try {
-            view?.findViewById<EditText>(R.id.category_task)?.text.toString().toLong()
+            binding.categoryTask.text.toString().toLong()
         } catch (e: NumberFormatException) {
             Toast.makeText(requireContext(), "Not valid category number", Toast.LENGTH_SHORT).show()
             1L
         }
 
         if (taskId != null && taskId != -1L) {
-            val editedTask = TaskEntity(taskId!!, title, description, 0L, category)
+            val editedTask = TaskEntity(taskId!!, title, description, dueDate, category)
             viewModel.updateTaskInDB(editedTask)
         } else {
             val newTask = TaskEntity(
@@ -116,5 +126,33 @@ class TaskDetailFragment : Fragment() {
     private fun navigateToCategoryFragment() {
         val action = TaskDetailFragmentDirections.actionTaskDetailFragmentToCategoryFragment()
         findNavController().navigate(action)
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(requireContext(), { _, selectedYear, selectedMonth, selectedDay ->
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(selectedYear, selectedMonth, selectedDay)
+            this.selectedDate = selectedDate.timeInMillis
+            updateDateTextView(selectedDate)
+        }, year, month, day)
+
+        datePickerDialog.show()
+    }
+
+    private fun updateDateTextView(selectedDate: Calendar) {
+        val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        val formattedDate = dateFormat.format(selectedDate.time)
+        binding.dueDate.text = formattedDate
+    }
+
+    fun formatDateFromLong(dateInMillis: Long): String {
+        val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        val date = Date(dateInMillis)
+        return dateFormat.format(date)
     }
 }
